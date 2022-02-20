a14a82eef9aad7f677fc403b5de2a7694fd7d3a1
ausin107 <78475645+ausin107@users.noreply.github.com>
2022-02-20 15:17:13 +0700

    change syntax

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import { useSelector, useDispatch } from "react-redux"
-import {useState, useEffect} from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import {plusAction, subAction, mulAction, divAction, addHistory} from '../action/reducer1'
 // page nào cũng có thể dùng các reducer đã combine ở trong store chỉ cần sử dụng các hook cơ bản của react-redux
@@ -13,43 +12,48 @@ function HomePage() {
         disPatch(plusAction())
         disPatch(addHistory({
             id: uuidv4(),
-            value: data
+            value: data,
+            type: 'Cộng 1'
         }))
     }
     const handleSub = (data) => {
         disPatch(subAction())
         disPatch(addHistory({
             id: uuidv4(),
-            value: data
+            value: data,
+            type: 'Trừ 1' 
         }))
     }
     const handleMul = (data) => {
         disPatch(mulAction())
         disPatch(addHistory({
             id: uuidv4(),
-            value: data
+            value: data,
+            type: 'Nhân 2'
         }))
     }
     const handleDiv = (data) => {
         disPatch(divAction())
         disPatch(addHistory({
             id: uuidv4(),
-            value: data
+            value: data,
+            type: 'Chia 2'
         }))
     }
     console.log(state)
     console.log(state2)
     return (
             <>
-            <h1>{state.value}</h1>
-            <button onClick={() => handlePlus(state.value)}>Plus Action</button>
-            <button onClick={() => handleSub(state.value)}>Subtract Action</button>
-            <button onClick={() => handleMul(state.value)}>Multiply Action</button>
-            <button onClick={() => handleDiv(state.value)}>Divice Action</button>
+            <h1>{state}</h1>
+            <button onClick={() => handlePlus(state)}>Plus Action</button>
+            <button onClick={() => handleSub(state)}>Subtract Action</button>
+            <button onClick={() => handleMul(state)}>Multiply Action</button>
+            <button onClick={() => handleDiv(state)}>Divice Action</button>
             <ul>
                 HISTORY CACULATOR
-                {state2.map(state => <li key={state.id}>
+                {state2.map(state => <li key={state.id} style={{fontSize: '20px'}}>
                     {state.value}
+                    <span style={{marginLeft: '10px'}}>{state.type}</span>
                 </li>)}
             </ul>
             </>
--- src/reducers/reducer1.js
+++ src/reducers/reducer1.js
@@ -1,31 +1,16 @@
 // tạo reducer để hiện thực các hàm logic khi dùng vs redux
-const initialSate = {
-    id: 0,
-    value: 0
-}
+const initialSate = 0;
 //initial state khi la obj phải update theo kiểu immuation
 const reducer1 = (state = initialSate, action) => {
     switch (action.type) {
         case 'PLUS_ACTION':
-            return {
-                ...state,
-                value: state.value + 1
-            }
+            return state + 1
         case 'SUB_ACTION':
-            return {
-                ...state,
-                value: state.value - 1
-            }
+            return state - 1
         case 'MUL_ACTION':
-            return {
-                ...state,
-                value: state.value * 2
-            }
+            return state * 2
         case 'DIV_ACTION':
-            return {
-                ...state,
-                value: state.value / 2
-            }
+            return state / 2
         default:
             return state
     }
